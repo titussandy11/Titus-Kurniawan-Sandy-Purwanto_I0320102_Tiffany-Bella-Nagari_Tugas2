0daf768ea6c5274ccb31402dde331c250f5083ab
Titus Kurniawan Sandy <titussandy11@gmail.com>
2021-03-11 12:16:52 +0700

    Tugas exercise 2.2

---
+++ I0320102_exercise2.2.py
@@ -0,0 +1,38 @@
+#Titus Kurniawan Sandy Purwanto
+#NIM I0320102
+#Kelas C
+#Exercise 2.2
+
+#proses memasukan data ke dalam variabel
+nama = "Titus Kurniawan Sandy Purwanto"
+#proses mencetak variabel
+print(nama)
+
+#nilai dan tipe data dalam variabel dapat diubah
+umur = 18               #nilai awal
+print(umur)             #mencetak nilai umur
+type(umur)              #mengecek tipe data umur
+umur = "delapan belas"  #nilai setelah diubah
+print(umur)             #mencetak nilai umur
+type(umur)              #mengecek tipe data umur
+
+namaDepan = "Titus Kurniawan"
+namaBelakang = "Sandy Purwanto"
+umur = 18
+hobi = "mendengarkan musik"
+print("Biodata\n", nama, "\n", umur, "\n", hobi)
+
+#contoh variabel lainya
+inivariabel = "Halo"
+ini_juga_variabel = "Hai"
+_inivariabeljuga = "Hi"
+inivariabel222 = "Bye"
+
+panjang = 10
+lebar = 5
+luas = panjang*lebar
+print(luas)
+
+
+
+
