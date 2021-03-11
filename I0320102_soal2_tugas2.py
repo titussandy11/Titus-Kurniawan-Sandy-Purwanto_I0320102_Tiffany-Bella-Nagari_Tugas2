90fe7f0df1de3ee65e918593b81f10bdbe9fdb62
Titus Kurniawan Sandy <titussandy11@gmail.com>
2021-03-11 22:50:20 +0700

    Soal 2

--- I0320102_soal2_tugas2.py
+++ I0320102_soal2_tugas2.py
@@ -1,1 +1,34 @@
+#Titus Kurniawan Sandy Purwanto
+#I0320102
+#Kelas C
 
+#Soal Latihan Nomor 2
+print("Biodata Mahasiswa")
+nama = "Titus Kurniawan Sandy Purwanto"
+umur = 18
+tempat_lahir = "Surakarta"
+tanggal_lahir = "28 Juni 2002"
+alamat = "Gebang RT 06 RW 11 Banjarsari, Surakarta"
+tinggi_badan = 185
+berat_badan = 78.5
+ukuran_sepatu = 45
+kampus = "Universitas Sebelas Maret"
+jurusan = "Teknik Industri"
+jajanan_favorit = "batagor, siomay, cilok"
+makanan_favorit = "Nasi goreng"
+minuman_favorit = "es teh, susu, kopi, dan air putih"
+hobi = "olahraga, mendengarkan musik, dan menonton film atau series"
+cita_cita = "ayah yang baik dan pengusaha"
+
+print("Halo teman-teman, nama saya",nama)
+print("umur saya", umur, "tahun")
+print("saya lahir di Kota", tempat_lahir, "pada tanggal", tanggal_lahir)
+print("saya tinggal di rumah orang tua saya yang beralamat di", alamat)
+print("tinggi badan saya", tinggi_badan, "cm", "dan berat badan saya", berat_badan, "kg")
+print("saya memakai sepatu berukuran", ukuran_sepatu)
+print("saat ini saya masih kuliah di", kampus, "jurusan", jurusan)
+print("jajanan favorit saya adalah", jajanan_favorit)
+print("saya sangat suka makan", makanan_favorit)
+print("saya memiliki beberapa minuman favorit, seperti", minuman_favorit)
+print("di sela-sela waktu kuliah, saya suka melakukan", hobi)
+print("cita-cita saya adalah menjadi", cita_cita)
