039d935a35cd633eb7effb2911b44cce9e75f480
Titus Kurniawan Sandy <titussandy11@gmail.com>
2021-03-11 12:00:58 +0700

    Tugas Exercise 2.1

--- I0320102_exercise2.1.py
+++ I0320102_exercise2.1.py
@@ -1,1 +1,35 @@
+# Titus Kurniawan Sandy Purwanto
+# NIM I0320102
+# Kelas C
+# Exercise 2.1
+
+# Tipe data Boolean
+a = True
+print(a)
+print(type(a))
+
+# Tipe data String
+a = "Saya sangat senang belajar Python"
+print(a)
+print(type(a))
+
+# Tipe data Integer
+a = 30
+print(a)
+print(type(a))
+
+# Tipe data Float
+a = 22,7
+print(a)
+print(type(a))
+
+# Tipe data Hexadecimal
+x = 9a
+print(x)
+print(type(x))
+
+# Tipe data Complex
+x = 5j
+print(x)
+print(type(x))
 
