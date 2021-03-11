960717b15747e9ee92a42411dc316b1a43b09172
Titus Kurniawan Sandy <titussandy11@gmail.com>
2021-03-11 12:52:51 +0700

    Tugas exercise 2.4

---
+++ I0320102_exercise2.4.py
@@ -0,0 +1,18 @@
+#Titus Kurniawan Sandy Purwanto
+#I0320102
+#Kelas C
+#Exercise 2.4
+
+#menampilkan informasi program
+
+print("Konversi suhu (Fahrenheit ke Celcius)")
+
+#input suhu dalam fahrenheit
+F = float(input("Masukkan suhu (fahrenheit): "))
+
+#melakukan konversi suhu ke celcius
+C = 5 * (F-32) / 9
+
+#menampilkan hasil konversi ke layar
+print("Fahrenheit: ", F)
+print("Celcius: ", C)
